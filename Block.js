154b7716c4b699af5e7e5677a3226d11ba5d4c75
vaghbadalkumar <80893399+vaghbadalkumar@users.noreply.github.com>
2021-05-10 09:25:54 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,34 @@
+class Block {
+  constructor(x, y, width, height) {
+      var options = {
+          isStatic: false,
+          density: 0.001,
+          friction: 1
+      }
+      this.visibility = 255;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+  }
+  display() {
+      
+      
+      if (this.body.speed < 3) {
+         var pos = this.body.position;
+         var angle = this.angle;
+         push();
+         translate(pos.x, pos.y);  
+         rotate(angle);
+         rectMode(CENTER);
+         rect(0, 0, this.width, this.height);
+          pop();
+      }  else {
+         World.remove(world, this.body);  
+         push();
+         this.visibility = this.visibility - 5;
+         // tint(255, this.visibility);
+          pop();
+      }
+  }
+}
